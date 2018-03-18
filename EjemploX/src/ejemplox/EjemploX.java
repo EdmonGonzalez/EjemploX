afafbad94dbf7f9c663cdc5c305865dd1b747432
Edmundo <Edmundo@172.16.12.100>
2018-03-18 08:51:32 -0600

    este es un mensaje de una nueva actualizacion

--- EjemploX/src/ejemplox/EjemploX.java
+++ EjemploX/src/ejemplox/EjemploX.java
@@ -17,6 +17,8 @@ public class EjemploX {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        
     }
     
 }
