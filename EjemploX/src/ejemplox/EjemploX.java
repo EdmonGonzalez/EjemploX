29ed8a1f7d00da87cc61c897ce2489f3a150f26e
Héctor Edmundo González <37415854+EdmonGonzalez@users.noreply.github.com>
2018-03-18 08:53:48 -0600

    Update EjemploX.java
    Este es otro cambio y genera otra version

--- EjemploX/src/ejemplox/EjemploX.java
+++ EjemploX/src/ejemplox/EjemploX.java
@@ -18,6 +18,8 @@ public class EjemploX {
         // TODO code application logic here
         System.out.println("Hola mundo");
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo tercero");
+        
         
     }
     
